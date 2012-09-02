9cd4ae09f9dcf998b6429a966771c6547676033e
Shoubhik Bose <sbose78@gmail.com>
2012-09-02 11:35:35 +0530

    correction

--- wsgi/openshift/views.py
+++ wsgi/openshift/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render_to_response
 
 def home(request):
     return render_to_response('home/home.html')
-    
-def narraive(request):
+
+def narrative(request):
 	t=loader.get_template('home/test.html')
 	c=Context({
 
