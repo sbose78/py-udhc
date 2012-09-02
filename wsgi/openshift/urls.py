a160666d4065d7c07256f92d1c62ee6821b9e8c9
Shoubhik Bose <sbose78@gmail.com>
2012-09-02 12:23:18 +0530

    correction

--- wsgi/openshift/urls.py
+++ wsgi/openshift/urls.py
@@ -8,14 +8,10 @@ urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'openshift.views.home', name='home'),
     url(r'^narrative/', 'openshift.views.narrative', name='narrative'),
-
-    url(r'^/(?P<path>.*)$', 'django.views.static.serve',{'document_root':settings.STATIC_ROOT'),
-    
+    url(r'^/(?P<path>.*)$', 'django.views.static.serve',{'document_root':settings.STATIC_ROOT),
     # url(r'^openshift/', include('openshift.foo.urls')),
-
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
