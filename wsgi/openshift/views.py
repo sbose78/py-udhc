eb2a67b13d21bea04af12e314251de01f8fdeb3e
Shoubhik Bose <sbose78@gmail.com>
2012-09-02 11:50:22 +0530

    correction

--- wsgi/openshift/views.py
+++ wsgi/openshift/views.py
@@ -1,6 +1,7 @@
 import os
 from django.shortcuts import render_to_response
 from django.template import Context, loader
+from django.http import HttpResponse
 
 def home(request):
     return render_to_response('home/home.html')
