30187cc3b54998db7161a23a00c1bd45bbd5af8d
Shoubhik Bose <sbose78@gmail.com>
2012-09-04 19:56:22 +0530

    insert data test

--- wsgi/openshift/views.py
+++ wsgi/openshift/views.py
@@ -2,6 +2,8 @@ import os
 from django.shortcuts import render_to_response
 from django.template import Context, loader
 from django.http import HttpResponse
+from pymongo.connection import connection
+
 
 def home(request):
     return render_to_response('home/home.html')
@@ -14,6 +16,13 @@ def health_record(request):
 	return HttpResponse(t.render(c))    
 
 def new_narrative(request):
+
+	connection = Connection('mongodb://sbose78:ECDW=19YRS@staff.mongohq.com:10068/BOSE')
+	db=connection['BOSE']
+	collection = db['controller']
+	data={"a1":"b", "b1":"b"}
+	collection.insert(data)
+	
 	t=loader.get_template('home/new_narrative.html')
 	c=Context({
 
@@ -27,6 +36,7 @@ def health_case(request):
 		})
 	return HttpResponse(t.render(c))
 
-
-
+def process_health_case(request):
+	about = request.POST['about']
+	details = request.POST['details']
 
