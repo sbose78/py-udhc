2b7e674d7cf3290945b64d78c7c8ca39ee3a5f5a
Shoubhik Bose <sbose78@gmail.com>
2012-09-08 12:16:50 +0530

    2

--- wsgi/openshift/views.py
+++ wsgi/openshift/views.py
@@ -32,9 +32,13 @@ def process_health_case(request):
 	details = request.POST['narrative_text']
 	image=request.FILES['image_scan']
 	connection = Connection('mongodb://sbose78:ECDW=19YRS@staff.mongohq.com:10068/BOSE')
-	db=connection['BOSE']
-	fs=gridfs.GridFS(db)
-	fs.put(image,filename="image_scan2")
+
+	if images:
+		db=connection['BOSE']
+		fs=gridfs.GridFS(db)
+		fs.put(image,filename="image_scan2")
+	else:
+		#nothing
 	return render_to_response('home/new_narrative.html',{ }, context_instance=RequestContext(request))
 	#collection = db['controller']
 	#data={"a1":about, "b1": details}
