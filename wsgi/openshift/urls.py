d3082b7bc28fedf0df1ba204982006d159af848b
Shoubhik Bose <sbose78@gmail.com>
2012-09-12 01:28:08 +0530

    r12

--- wsgi/openshift/urls.py
+++ wsgi/openshift/urls.py
@@ -6,10 +6,12 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'openshift.views.home', name='home'),
+    url(r'^new_health_report.udhc', 'openshift.views.new_health_report', name='new_health_report'),
     url(r'^new_narrative.udhc', 'openshift.views.new_narrative', name='new_narrative'),
     url(r'^health_case.udhc', 'openshift.views.health_case', name='health_case'),
     url(r'^submit_health_case.udhc', 'openshift.views.process_health_case', name='process_health_case'),
     url(r'^health_record.udhc', 'openshift.views.health_record', name='health_record'),
+    url(r'^add_more_reports.udhc', 'openshift.views.add_more_reports', name='add_more_reports'),
     url(r'^my_image/(?P<image_id>.*)$', 'openshift.views.my_image', name='my_image'),
     url(r'^(?P<path>.*)$', 'django.views.static.serve',{'document_root': settings.STATIC_ROOT }),
     # url(r'^openshift/', include('openshift.foo.urls')),
