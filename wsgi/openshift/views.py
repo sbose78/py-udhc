63f0c2e686b286b8f1cef513ccba1ea4da714cde
Shoubhik Bose <sbose78@gmail.com>
2012-10-07 01:45:35 +0530

    adding translietrate 3

--- wsgi/openshift/views.py
+++ wsgi/openshift/views.py
@@ -145,14 +145,7 @@ def process_health_case(request):
 		file_id = fs.put(image,filename="about")
 		data={ "about":about, "file_id":file_id , "name" : sci_name }
 		collection.insert(data)
-	elif details_local != "":
-		data={
-			"about":about,
-			"details":details_local,
-			"name" : sci_name , 
-			"time": datetime.datetime.utcnow()
-		}
-		collection.insert(data)	
+		details = details + " " +details_local
 	else:
 		data={
 			"about":about,
